04ce8ade776c56a6db3c54c6391de963c8a2a9db
向内延伸的对数螺线 <3253615697@qq.com>
2023-09-24 12:11:00 +0800

    增加了用于测试近战序列的武器 并且增加了两个近战模式 依旧只有壳

--- CodeLibrary/DataStructures/MeleeStrustures.cs
+++ CodeLibrary/DataStructures/MeleeStrustures.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader.UI;
 
 namespace LogSpiralLibrary.CodeLibrary.DataStructures
 {
@@ -33,9 +35,13 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
     public interface IMeleeAttackData
     {
         //持续时间 角度 位移 修改数据
-
+        /// <summary>
+        /// 近战数据修改
+        /// </summary>
         MeleeModifyData ModifyData => new MeleeModifyData();
-
+        /// <summary>
+        /// 执行次数
+        /// </summary>
         int Cycle => 1;
         /// <summary>
         /// 中心偏移量，默认零向量
@@ -49,16 +55,50 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
         /// 当前周期的进度
         /// </summary>
         float Factor { get; }
-
+        /// <summary>
+        /// 旋转量
+        /// </summary>
         float Rotation { get; }
-
+        /// <summary>
+        /// 大小
+        /// </summary>
         float Size { get; }
-
+        /// <summary>
+        /// 是否具有攻击性
+        /// </summary>
         bool Attacktive { get; }
 
+        /// <summary>
+        /// 被切换时调用,脉冲性
+        /// </summary>
+        void OnActive();
+
+        /// <summary>
+        /// 被换走时调用,脉冲性
+        /// </summary>
+        void OnDeactive();
+
+        /// <summary>
+        /// 结束时调用,脉冲性
+        /// </summary>
         void OnEndAttack();
 
+        /// <summary>
+        /// 开始攻击时调用,脉冲性
+        /// </summary>
+        void OnStartAttack();
+
+        /// <summary>
+        /// 攻击期间调用,持续性
+        /// </summary>
+        void OnAttack();
+
+        /// <summary>
+        /// 攻击以外时间调用,持续性
+        /// </summary>
+        void OnCharge();
         Condition Condition { get; }
+        void Update(ref int timer, out bool canReduceTimer);
     }
 
     public class MeleeSequence
@@ -66,24 +106,143 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
         public class MeleeGroup
         {
             public List<IMeleeAttackData> meleeAttackDatas = new List<IMeleeAttackData>();
-            public IMeleeAttackData GetCurrentMeleeData() 
+            public IMeleeAttackData GetCurrentMeleeData()
             {
                 IMeleeAttackData result = null;
                 if (meleeAttackDatas == null || meleeAttackDatas.Count == 0) goto Label;
-                foreach (var meleeAtack in meleeAttackDatas) 
+                foreach (var meleeAtack in meleeAttackDatas)
                 {
-                    if(meleeAtack.Condition.IsMet())
+                    if (meleeAtack.Condition.IsMet())
                         result = meleeAtack;
                 }
                 result ??= meleeAttackDatas.FirstOrDefault();
-                Label:
+            Label:
                 return result;
             }
+            public string GroupName = "My MeleeGroup";
         }
-        public List<MeleeGroup> meleeGroups = new List<MeleeGroup>();
+        /// <summary>
+        /// 把两个类打包在一个类的实用寄巧
+        /// </summary>
+        public class MeleeGSWraper
+        {
+            public readonly MeleeGroup groupInfo;
+            public readonly MeleeSequence sequenceInfo;
+
+            public bool IsGroup => groupInfo != null;
+            public bool IsSequence => sequenceInfo != null;
+            public string Name => groupInfo?.GroupName ?? sequenceInfo.SequenceName ?? "null";
+            public List<MeleeGroup> GetMeleeGroups()
+            {
+                var result = new List<MeleeGroup>();
+                if (IsGroup)
+                {
+                    result.Add(groupInfo);
+                }
+                else if (IsSequence)
+                {
+                    foreach (var wrap in sequenceInfo.meleeWraps)
+                        result.AddRange(wrap.GetMeleeGroups());
+                }
+                return result;
+            }
+            public MeleeGSWraper(MeleeGroup group)
+            {
+                groupInfo = group;
+            }
+
+            public MeleeGSWraper(MeleeSequence sequence)
+            {
+                sequenceInfo = sequence;
+            }
+            public static implicit operator MeleeGSWraper(MeleeGroup group) => new MeleeGSWraper(group);
+            public static implicit operator MeleeGSWraper(MeleeSequence sequence) => new MeleeGSWraper(sequence);
+
+            public bool ContainsSequence(MeleeSequence meleeSequence) => ContainsSequence(meleeSequence.GetHashCode());
+            public bool ContainsSequence(int hashCode)
+            {
+                if (IsGroup) return false;
+                if (IsSequence)
+                {
+                    if (sequenceInfo.GetHashCode() == hashCode) return true;
+                    foreach (var wraps in sequenceInfo.meleeWraps)
+                    {
+                        if (wraps.ContainsSequence(hashCode)) return true;
+                    }
+                }
+                return false;
+            }
+        }
+        /// <summary>
+        /// 重新导出动作序列
+        /// </summary>
+        public void Recalculate()
+        {
+            resultGroups.Clear();
+            foreach (var wrap in meleeWraps)
+                resultGroups.AddRange(wrap.GetMeleeGroups());
+            counter = 0;
+            timer = 0;
+        }
+        public void Add(MeleeGSWraper wraper)
+        {
+            if (wraper.ContainsSequence(this))
+            {
+                Main.NewText("不可调用自己");
+                return;
+            }
+            meleeWraps.Add(wraper);
+            Recalculate();
+        }
+        public void Insert(int index, MeleeGSWraper wraper)
+        {
+            if (wraper.ContainsSequence(this))
+            {
+                Main.NewText("不可调用自己");
+                return;
+            }
+            meleeWraps.Insert(index, wraper);
+            Recalculate();
+
+        }
+        public List<MeleeGroup> resultGroups = new List<MeleeGroup>();
+        /// <summary>
+        /// 请不要对这个列表直接add
+        /// </summary>
+        public List<MeleeGSWraper> meleeWraps = new List<MeleeGSWraper>();
         public IMeleeAttackData currentData;
+        public string SequenceName = "My MeleeSequence";
+        public int counter;
+        public int timer;
+        public bool Attacktive;
+        public void Update(Player player)
+        {
+            if (timer <= 0 || currentData == null)
+            {
+                if (currentData != null) currentData.OnDeactive();
+                currentData = resultGroups[counter % resultGroups.Count].GetCurrentMeleeData();
+                timer = (int)(player.itemAnimationMax * currentData.ModifyData.actionOffsetSpeed);
+                counter++;
+            }
+            if (currentData != null)
+            {
+                bool oldValue = Attacktive;
+                Attacktive = currentData.Attacktive;
+                if (!oldValue && Attacktive)
+                {
+                    currentData.OnStartAttack();
+                }
+                if (oldValue && !Attacktive)
+                    currentData.OnEndAttack();
+                if (Attacktive) currentData.OnAttack();
+                else currentData.OnCharge();
+                currentData.Update(ref timer, out bool flag);
+                if (flag)
+                    timer--;
+            }
+        }
     }
-    public struct SwooshInfo : IMeleeAttackData
+    public class SwooshInfo : IMeleeAttackData
     {
         public bool negativeDir = false;
         public float kValue = 1;
@@ -92,7 +251,7 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
         {
         }
 
-        public float Factor => throw new NotImplementedException();
+        public virtual float Factor => throw new NotImplementedException();
 
         public float Rotation => throw new NotImplementedException();
 
@@ -100,15 +259,44 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
 
         public bool Attacktive => throw new NotImplementedException();
 
-        public Condition Condition { get; set; }
+        public Condition Condition { get; set; } = new Condition("Always", () => true);
 
+        public virtual void OnAttack()
+        {
+            throw new NotImplementedException();
+        }
 
-        public void OnEndAttack()
+        public virtual void OnCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnEndAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnStartAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnDeactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Update(ref int timer, out bool canReduceTimer)
         {
             throw new NotImplementedException();
         }
     }
-    public struct StabInfo : IMeleeAttackData 
+    public class StabInfo : IMeleeAttackData
     {
         public bool negativeDir = false;
         public float kValue = 1;
@@ -116,7 +304,84 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
         public StabInfo()
         {
         }
+        public int Cycle { get => realCycle; set => givenCycle = value; }
+        protected int realCycle;
+        protected int givenCycle;
+        public virtual float Factor => throw new NotImplementedException();
 
+        public float Rotation => throw new NotImplementedException();
+
+        public float Size => throw new NotImplementedException();
+
+        public bool Attacktive => throw new NotImplementedException();
+
+        public Condition Condition { get; set; } = new Condition("Always", () => true);
+
+        public Vector2 offsetCenter => throw new NotImplementedException();
+
+        public virtual void OnAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnEndAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnStartAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnDeactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Update(ref int timer, out bool canReduceTimer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class RapidlyStabInfo : StabInfo
+    {
+        public (int min, int max) CycleOffsetRange 
+        {
+            get => range;
+            set 
+            {
+                var v = value;
+                v.min = Math.Clamp(v.min, int.MinValue, v.max);
+                v.max = Math.Clamp(v.max, v.min, int.MaxValue);
+                range = v;
+            }
+        }
+        (int, int) range;
+        void ResetCycle() => realCycle = Math.Clamp(givenCycle + Main.rand.Next(range.Item1, range.Item2), 1, int.MaxValue);
+        public override void OnActive()
+        {
+            ResetCycle();
+            base.OnActive();
+        }
+        public override void OnEndAttack()
+        {
+            ResetCycle();
+            base.OnEndAttack();
+        }
+    }
+    public class ConvoluteInfo : IMeleeAttackData
+    {
         public float Factor => throw new NotImplementedException();
 
         public float Rotation => throw new NotImplementedException();
@@ -125,11 +390,86 @@ namespace LogSpiralLibrary.CodeLibrary.DataStructures
 
         public bool Attacktive => throw new NotImplementedException();
 
-        public Condition Condition { get; set; }
+        public Condition Condition => throw new NotImplementedException();
 
-        public Vector2 offsetCenter => throw new NotImplementedException();
+        public void OnActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDeactive()
+        {
+            throw new NotImplementedException();
+        }
 
         public void OnEndAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStartAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ref int timer, out bool canReduceTimer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class ShockingDashInfo : IMeleeAttackData
+    {
+        public float Factor => throw new NotImplementedException();
+
+        public float Rotation => throw new NotImplementedException();
+
+        public float Size => throw new NotImplementedException();
+
+        public bool Attacktive => throw new NotImplementedException();
+
+        public Condition Condition => throw new NotImplementedException();
+
+        public void OnActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDeactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEndAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStartAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ref int timer, out bool canReduceTimer)
         {
             throw new NotImplementedException();
         }
---
+++ ForFun/TestBlade/TestBlade.cs
@@ -0,0 +1,85 @@
+﻿using LogSpiralLibrary.CodeLibrary.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSpiralLibrary.ForFun.TestBlade
+{
+    public class TestBlade : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.height = Item.width = 69;
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 50;
+            Item.knockBack = 5f;
+            Item.autoReuse = true;
+            Item.channel = true;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.shoot = ModContent.ProjectileType<TestBladeProj>();
+            Item.shootSpeed = 1f;
+            base.SetDefaults();
+        }
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] == 0;
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+        }
+    }
+    public class TestBladeProj : ModProjectile
+    {
+        public override string Texture => base.Texture.Replace("Proj", "");
+        public MeleeSequence meleeSequence = new MeleeSequence();
+        public IMeleeAttackData currentData => meleeSequence.currentData;
+        public override void SetDefaults()
+        {
+            Projectile.timeLeft = 10;
+            Projectile.width = Projectile.height = 1;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Melee;
+            RapidlyStabInfo stabInfo = new RapidlyStabInfo();
+            SwooshInfo swooshInfo = new SwooshInfo();
+
+            MeleeSequence.MeleeGroup groupStab = new MeleeSequence.MeleeGroup();
+            meleeSequence.Add(groupStab);
+            MeleeSequence.MeleeGroup groupSlash = new MeleeSequence.MeleeGroup();
+            meleeSequence.Add(groupStab);
+            MeleeSequence.MeleeGroup groupShoot = new MeleeSequence.MeleeGroup();
+            meleeSequence.Add(groupStab);
+            base.SetDefaults();
+        }
+        Player player => Main.player[Projectile.owner];
+        public override void AI()
+        {
+            Projectile.damage = player.GetWeaponDamage(player.HeldItem);
+            Projectile.Center = player.Center + currentData.offsetCenter;
+            meleeSequence.Update(player);
+            base.AI();
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            spriteBatch.Draw(TextureAssets.Projectile[Projectile.type].Value,
+                player.Center - Main.screenPosition + currentData.offsetCenter,
+                null, Color.White, currentData.Rotation - MathHelper.PiOver4, 
+                currentData.offsetOrigin, currentData.ModifyData.actionOffsetSize, 0, 0);
+            return base.PreDraw(ref lightColor);
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            if (!currentData.Attacktive) return false;
+            float point = 0f;
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center,
+                Projectile.Center + currentData.Rotation.ToRotationVector2() * currentData.ModifyData.actionOffsetSize * TextureAssets.Projectile[Projectile.type].Size().Length(), 48f, ref point);
+        }
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            modifiers.Knockback *= meleeSequence.currentData.ModifyData.actionOffsetKnockBack;
+            base.ModifyHitNPC(target, ref modifiers);
+        }
+
+    }
+}
